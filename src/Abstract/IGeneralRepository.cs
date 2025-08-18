8071ec5b7b6858223b5694dbe8f74a0102baa33d
Jake Soenneker <jake@soenneker.com>
2025-08-18 09:35:29 -0500

    -> SharedRepo

--- src/Abstract/IGeneralRepository.cs
+++ src/Abstract/IGeneralRepository.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Threading;
-using System.Threading.Tasks;
-using Soenneker.Cosmos.Repository.Abstract;
-using Soenneker.Dtos.IdPartitionPair;
+﻿using Soenneker.Cosmos.Repositories.Shared.Abstract;
 
 namespace Soenneker.Cosmos.Repositories.General.Abstract;
 
@@ -11,18 +6,6 @@ namespace Soenneker.Cosmos.Repositories.General.Abstract;
 /// A data persistence abstraction layer for Cosmos DB General type documents
 /// </summary>
 /// <typeparam name="TDocument"></typeparam>
-public interface IGeneralRepository<TDocument> : ICosmosRepository<TDocument> where TDocument : class
+public interface IGeneralRepository<TDocument> : ISharedRepository<TDocument> where TDocument : class
 {
-    [Pure]
-    new ValueTask<List<TDocument>> GetAll(double? delayMs, CancellationToken cancellationToken = default);
-
-    [Pure]
-    new ValueTask<bool> Any(CancellationToken cancellationToken = default);
-
-    [Pure]
-    new ValueTask<List<IdPartitionPair>> GetAllIds(double? delayMs, CancellationToken cancellationToken = default);
-
-    new ValueTask DeleteAll(double? delayMs = null, bool useQueue = false, CancellationToken cancellationToken = default);
-
-    new ValueTask DeleteAllPaged(int pageSize, double? delayMs, bool useQueue, CancellationToken cancellationToken = default);
 }
--- src/GeneralRepository.cs
+++ src/GeneralRepository.cs
@@ -1,88 +1,21 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Soenneker.Constants.Data;
 using Soenneker.Cosmos.Container.Abstract;
 using Soenneker.Cosmos.Repositories.General.Abstract;
-using Soenneker.Cosmos.Repository;
+using Soenneker.Cosmos.Repositories.Shared;
 using Soenneker.Documents.General;
-using Soenneker.Dtos.IdPartitionPair;
-using Soenneker.Extensions.ValueTask;
 using Soenneker.Utils.BackgroundQueue.Abstract;
-using Soenneker.Utils.Method;
 using Soenneker.Utils.UserContext.Abstract;
 
 namespace Soenneker.Cosmos.Repositories.General;
 
 ///<inheritdoc cref="IGeneralRepository{TDocument}"/>
-public abstract class GeneralRepository<TDocument> : CosmosRepository<TDocument>, IGeneralRepository<TDocument> where TDocument : GeneralDocument
+public abstract class GeneralRepository<TDocument> : SharedRepository<TDocument>, IGeneralRepository<TDocument> where TDocument : GeneralDocument
 {
     public override string ContainerName => "general";
 
-    protected abstract string EntityType { get; }
-
-    protected GeneralRepository(ICosmosContainerUtil cosmosContainerUtil, IConfiguration config, ILogger<CosmosRepository<TDocument>> logger,
+    protected GeneralRepository(ICosmosContainerUtil cosmosContainerUtil, IConfiguration config, ILogger<GeneralRepository<TDocument>> logger,
         IUserContext userContext, IBackgroundQueue backgroundQueue) : base(cosmosContainerUtil, config, logger, userContext, backgroundQueue)
     {
-    }
-
-    public override async ValueTask<List<TDocument>> GetAll(double? delayMs = null, CancellationToken cancellationToken = default)
-    {
-        IQueryable<TDocument> query = await BuildQueryable(null, cancellationToken).NoSync();
-
-        query = query.Where(c => c.EntityType == EntityType);
-
-        return await GetItems(query, delayMs, cancellationToken).NoSync();
-    }
-
-    public override async ValueTask<List<IdPartitionPair>> GetAllIds(double? delayMs = null, CancellationToken cancellationToken = default)
-    {
-        IQueryable<TDocument> query = await BuildQueryable(null, cancellationToken);
-
-        query = query.Where(c => c.EntityType == EntityType);
-
-        return await GetIds(query, delayMs, cancellationToken).NoSync();
-    }
-
-    public override async ValueTask DeleteAll(double? delayMs = null, bool useQueue = false, CancellationToken cancellationToken = default)
-    {
-        Logger.LogWarning("-- COSMOS: {method} (General.{type}Document) w/ {delayMs}ms delay between docs", MethodUtil.Get(), EntityType,
-            delayMs.GetValueOrDefault());
-
-        List<IdPartitionPair> ids = await GetAllIds(delayMs, cancellationToken).NoSync();
-
-        await DeleteIds(ids, delayMs, useQueue, cancellationToken).NoSync();
-
-        Logger.LogDebug("-- COSMOS: Finished {method} (General.{type}Document)", MethodUtil.Get(), EntityType);
-    }
-
-    public override async ValueTask DeleteAllPaged(int pageSize = DataConstants.DefaultCosmosPageSize, double? delayMs = null, bool useQueue = false,
-        CancellationToken cancellationToken = default)
-    {
-        Logger.LogWarning("-- COSMOS: {method} (General.{type}) w/ {delayMs}ms delay between docs", MethodUtil.Get(), typeof(TDocument).Name,
-            delayMs.GetValueOrDefault());
-
-        IQueryable<TDocument> query = await BuildPagedQueryable(pageSize, cancellationToken: cancellationToken).NoSync();
-        query = query.Where(c => c.EntityType == EntityType);
-        query = query.OrderBy(c => c.CreatedAt);
-
-        var newQuery = query.Select(c => new {c.DocumentId, c.PartitionKey});
-
-        await ExecuteOnGetItemsPaged(newQuery, async results =>
-        {
-            Logger.LogDebug("Number of rows to be deleted in page: {rows}", results.Count);
-
-            foreach (var result in results)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                await DeleteItem(result.DocumentId, result.PartitionKey, useQueue, cancellationToken).NoSync();
-            }
-        }, cancellationToken).NoSync();
-
-        Logger.LogDebug("-- COSMOS: Finished {method} (General.{type})", MethodUtil.Get(), typeof(TDocument).Name);
     }
 }
